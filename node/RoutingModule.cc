ba4ac0cd09ec486ab80f2f92e82024fa2d276dfa
alfonso <aarizaq_m@hotmail.com>
2018-07-03 11:35:46 +0200

    Include data check correction

--- node/RoutingModule.cc
+++ node/RoutingModule.cc
@@ -302,6 +302,13 @@ void RoutingModule::procActualize(Actualize *pkt)
             dijkstra->deleteEdge(nodeId, linkData.node);
         }
         else {
+            if (linkData.max > linkData.nominal)
+                throw cRuntimeError("Data error max");
+            if (linkData.mean > linkData.nominal)
+                throw cRuntimeError("Data error max");
+            if (linkData.min > linkData.nominal)
+                throw cRuntimeError("Data error max");
+
             double minResidual = linkData.nominal - linkData.max;
             double meanResidual = linkData.nominal - linkData.mean;
             double maxResidual = linkData.nominal - linkData.min;
@@ -357,8 +364,6 @@ void RoutingModule::procActualize(Actualize *pkt)
 
             }
 
-            if (minResidual == 0)
-                throw cRuntimeError("Problems detected");
             dijFuzzy->addEdge(nodeId, linkData.node, minResidual, meanResidual, maxResidual);
             if (par("instValue").boolValue()) {
                 if (rType == SW || rType == WS)
